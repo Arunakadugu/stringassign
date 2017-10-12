568f506a94d3192753c5424e3ca21d0b8881c625
Aruna kadugu <arunakumari.7k@gmail.com>
2017-10-12 09:49:24 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = " Lovely";
+        System.out.printf("your name:%s%n", name);
+
+
+
+    }
+}
